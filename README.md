b873b67993d2ae95ad3d9b160992a2a45cd530cd
Ethan Edwards <evanevansonbusiness@gmail.com>
2023-03-31 19:39:56 -0700

    Clear repository

--- README.md
+++
@@ -1,3 +0,0 @@
-# TopDownShmup
-
-Developed with Unreal Engine 5
--- Source/TopDownShmup/TopDownShmup.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "TopDownShmup.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDownShmup, "TopDownShmup" );
-
-DEFINE_LOG_CATEGORY(LogTopDownShmup)
- 
